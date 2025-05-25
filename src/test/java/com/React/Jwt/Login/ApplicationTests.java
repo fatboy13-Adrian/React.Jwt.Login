24a3eaa3606c4d340bb6c65c3c90133563ca5e81
fatboy13-Adrian <birdbird_dog@hotmail.sg>
2025-05-25 19:46:28 +0800

    Initial commit

---
+++ src/test/java/com/React/Jwt/Login/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.React.Jwt.Login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
